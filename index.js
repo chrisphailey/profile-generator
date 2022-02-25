0bcfcaec112af5dd8bb028f58822ceafe15bab9d
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-02-25 15:52:28 -0600

    prompts

---
+++ index.js
@@ -0,0 +1,125 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's name!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?",
+            validate: idInput => {
+                if (idInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's employee ID!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email address?",
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's email address!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is this employee's role?",
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'number',
+            message: "What is the team manager's office number?",
+            when: ({role}) => {
+                if (role === "Manager"){
+                    return true
+                } else {
+                    return false;
+                }
+            },
+            validate: numberInput => {
+                if (numberInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the team manager's office number!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub username?",
+            when: ({role}) => {
+                if (role === "Engineer"){
+                    return true
+                } else {
+                    return false
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's school?",
+            when: ({role}) => {
+                if (role === "Intern"){
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+    ]);
+}
+
+
+// Function to write HTML file
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./index.html', generateIndex(data), err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    })
+}
+
+// function to initialize app
+function init() {
+    promptUser()
+    // .then(data => {
+    //     return writeToFile(data);
+    // })
+}
+
+init();
